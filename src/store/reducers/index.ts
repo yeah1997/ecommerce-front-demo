e8afe932ca71b67e99ba08447735ccff2fc3e542
yeah1997 <lee7778889@gmail.com>
2023-05-18 21:15:28 +0900

    fix

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,17 +1,19 @@
-import { connectRouter, RouterState } from "connected-react-router"
-import { combineReducers } from "redux"
-import { History } from "history"
+import { connectRouter, RouterState } from "connected-react-router";
+import { combineReducers } from "redux";
+import { History } from "history";
 import authReducer, { AuthState } from "./auth.reducer";
+import { CategoryState } from "./category.reducer";
 
 export interface AppState {
-    router: RouterState,
-    auth: AuthState
+  router: RouterState;
+  auth: AuthState;
+  category: CategoryState;
 }
 
 const createRouterReducer = (history: History) =>
-    combineReducers({
-        router: connectRouter(history),
-        auth: authReducer
-    })
+  combineReducers({
+    router: connectRouter(history),
+    auth: authReducer,
+  });
 
-export default createRouterReducer
+export default createRouterReducer;
